4d22b559fcf4c0cc194590d42c47325d5c0fc810
chengying <hollye2558@gmail.com>
2022-11-02 10:24:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathMyDog");
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(noName="*"){
+    return function(adj1="special") {
+return `You are ${noName}${adj1}${noName}!`;
+    }
+    
+}
